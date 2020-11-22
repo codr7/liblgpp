564965e07648e2862b870e68f5e0d8db516e1f7a
Andreas Nilsson <andreas.nilsson@jayway.com>
2020-11-22 17:47:59 +0100

    add cp op

---
+++ src/lgpp/ops/cp.hpp
@@ -0,0 +1,24 @@
+#ifndef LGPP_OPS_CP_HPP
+#define LGPP_OPS_CP_HPP
+
+#include "../stack.hpp"
+#include "../val.hpp"
+#include "../op.hpp"
+
+namespace lgpp::ops {
+
+  struct Cp {
+    Cp(size_t offs = 0, size_t len = 1): offs(offs), len(len) {}
+    size_t offs, len;
+  };
+
+  template <>
+  inline const Op *eval(const Op &op, const Cp &imp, lgpp::Stack &stack) {
+    auto i = stack.end() - imp.offs - 1;
+    copy(i, i + imp.len, back_inserter(stack)); 
+    return &op+1;
+  }
+
+}
+
+#endif
--- src/tests.cpp
+++ src/tests.cpp
@@ -3,6 +3,7 @@
 #include "lgpp/label.hpp"
 #include "lgpp/ops/add.hpp"
 #include "lgpp/ops/branch_eq.hpp"
+#include "lgpp/ops/cp.hpp"
 #include "lgpp/ops/drop.hpp"
 #include "lgpp/ops/push.hpp"
 #include "lgpp/ops/stop.hpp"
@@ -16,6 +17,22 @@
 using namespace lgpp;
 
 Type<int> Int("Int");
+
+void vm_branch_tests(VM &vm) {
+  Stack s;
+
+  vm.thread().ops.reserve(10);
+  vm.emit<ops::Push>(Int, 1);
+  Label b;
+  vm.emit<ops::BranchEq>(b, Int, 1);
+  vm.emit<ops::Push>(Int, 2);
+  vm.emit<ops::Stop>();
+  b.pc = vm.last_op().pc;
+  vm.eval(0, s); 
+  assert(s.size() == 1);
+  assert(s.back().as(Int) == 1);
+  vm.clear_ops();
+}
 
 void vm_add_tests(VM &vm) {
   Stack s;
@@ -43,49 +60,72 @@ void vm_sub_tests(VM &vm) {
   vm.clear_ops();
 }
 
-void vm_branch_tests(VM &vm) {
-  Stack s;
-
-  vm.thread().ops.reserve(10);
-  vm.emit<ops::Push>(Int, 42);
-  Label b;
-  vm.emit<ops::BranchEq>(b, Int, 42);
-  vm.emit<ops::Push>(Int, 7);
-  vm.emit<ops::Stop>();
-  b.pc = vm.last_op().pc;
-  vm.eval(0, s); 
-  assert(s.size() == 1);
-  assert(s.back().as(Int) == 42);
-  vm.clear_ops();
+void vm_math_tests(VM &vm) {
+  vm_add_tests(vm);
+  vm_sub_tests(vm);
 }
 
-void vm_stack_tests(VM &vm) {
+void vm_stack_cp_tests(VM &vm) {
   Stack s;
 
-  vm.emit<ops::Push>(Int, 42);
-  vm.emit<ops::Push>(Int, 7);
+  vm.emit<ops::Push>(Int, 1);
+  vm.emit<ops::Push>(Int, 2);
+  vm.emit<ops::Push>(Int, 3);
+  vm.emit<ops::Cp>(2, 2);
+  vm.emit<ops::Stop>();
+  vm.eval(0, s);
+  
+  assert(s.size() == 5);
+  assert(pop(s).as(Int) == 2);
+  assert(pop(s).as(Int) == 1);
+  assert(pop(s).as(Int) == 3);
+  assert(pop(s).as(Int) == 2);
+  assert(pop(s).as(Int) == 1);
+  vm.clear_ops();  
+}
+
+void vm_stack_drop_tests(VM &vm) {
+  Stack s;
+
+  vm.emit<ops::Push>(Int, 1);
+  vm.emit<ops::Push>(Int, 2);
+  vm.emit<ops::Push>(Int, 3);
+  vm.emit<ops::Drop>(1, 2);
+  vm.emit<ops::Stop>();
+  vm.eval(0, s); 
+  assert(s.size() == 1);
+  assert(pop(s).as(Int) == 1);
+  vm.clear_ops();  
+}
+
+void vm_stack_swap_tests(VM &vm) {
+  Stack s;
+
+  vm.emit<ops::Push>(Int, 1);
+  vm.emit<ops::Push>(Int, 2);
   vm.emit<ops::Swap>();
   vm.emit<ops::Stop>();
   auto &stop(vm.last_op());
   assert(stop.pc == 3);
   assert(&vm.eval(0, s) == &stop); 
   assert(s.size() == 2);
-  assert(s.back().as(Int) == 42);
+  assert(pop(s).as(Int) == 1);
+  assert(pop(s).as(Int) == 2);
   vm.clear_ops();
-  
-  vm.emit<ops::Drop>(1, 2);
-  vm.emit<ops::Stop>();
-  vm.eval(0, s); 
-  assert(s.size() == 0);
-  vm.clear_ops();  
+}
+
+
+void vm_stack_tests(VM &vm) {
+  vm_stack_cp_tests(vm);
+  vm_stack_drop_tests(vm);
+  vm_stack_swap_tests(vm);
 }
 
 void vm_tests() {
   VM vm;
   
-  vm_add_tests(vm);
-  vm_sub_tests(vm);
   vm_branch_tests(vm);
+  vm_math_tests(vm);
   vm_stack_tests(vm);
 }
 
