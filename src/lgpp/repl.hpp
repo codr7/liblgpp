03a1b86e7f35fd28d4f202ee201d162da2988c20
Andreas Nilsson <andreas.nilsson@jayway.com>
2020-12-14 02:32:44 +0100

    repl struct

--- src/lgpp/repl.hpp
+++ src/lgpp/repl.hpp
@@ -2,18 +2,28 @@
 #define LGPP_REPL_HPP
 
 #include <iostream>
+#include <optional>
 #include <sstream>
 
 namespace lgpp {
 
   using namespace std;
   
-  void repl(istream &in, function<bool (const string &)> fn) {
+  struct REPL {
+    REPL(istream &in, ostream &out): in(in), out(out) {}
+
+    istream &in;
+    ostream &out;
+
+    optional<function<bool (const string &line)>> on_getline;
+  };
+  
+  inline void enter(REPL &repl) {
     stringstream buf;
     string line;
   
-    while (getline(in, line)) {
-      if (!fn(line)) { break; }
+    while (getline(repl.in, line)) {
+      if (repl.on_getline && !(*repl.on_getline)(line)) { break; }
     }
   }
 
